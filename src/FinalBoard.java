af75249fbabb6e04302a193cb2d406718aa38bad
Michael <jsuwncue2970@gmail.com>
2023-03-09 12:13:20 +0900

    no message

--- src/FinalBoard.java
+++ src/FinalBoard.java
@@ -13,53 +13,53 @@ public class FinalBoard {
 		System.out.println("lol");
 		System.out.println("lol");
 
-		// 게시판의 출력 명령 번호
+		// 寃뚯떆�뙋�쓽 異쒕젰 紐낅졊 踰덊샇
 		while (true) {
-			System.out.println("==== 게시판 ====");
-			System.out.println("1. 게시글 등록");
-			System.out.println("2. 게시글 삭제");
-			System.out.println("3. 게시글 목록 보기");
-			System.out.println("0. 종료");
-			System.out.println("숫자만 입력해주세요");
+			System.out.println("==== 寃뚯떆�뙋 ====");
+			System.out.println("1. 寃뚯떆湲� �벑濡�");
+			System.out.println("2. 寃뚯떆湲� �궘�젣");
+			System.out.println("3. 寃뚯떆湲� 紐⑸줉 蹂닿린");
+			System.out.println("0. 醫낅즺");
+			System.out.println("�닽�옄留� �엯�젰�빐二쇱꽭�슂");
 			
-			try {  // 오류 발생시 처음으로
+			try {  // �삤瑜� 諛쒖깮�떆 泥섏쓬�쑝濡�
 				
 				int menu = sc.nextInt();
 
-				if (menu == 1) { // 게시글 등록
-					System.out.println("제목을 입력하세요.");
-					sc.nextLine(); // '\n'을 버리기 위해 추가
+				if (menu == 1) { // 寃뚯떆湲� �벑濡�
+					System.out.println("�젣紐⑹쓣 �엯�젰�븯�꽭�슂.");
+					sc.nextLine(); // '\n'�쓣 踰꾨━湲� �쐞�빐 異붽�
 					String title = sc.nextLine();
-					System.out.println("내용을 입력하세요.");
+					System.out.println("�궡�슜�쓣 �엯�젰�븯�꽭�슂.");
 					String content = sc.nextLine();
-					int id = board.getPosts().size() + 1; // 게시글 ID는 현재 게시글 개수 + 1로 설정
+					int id = board.getPosts().size() + 1; // 寃뚯떆湲� ID�뒗 �쁽�옱 寃뚯떆湲� 媛쒖닔 + 1濡� �꽕�젙
 					Board2 post = new Board2(id, title, content);
 					board.addPost(post);
-					System.out.println("게시글이 등록되었습니다. ID: " + id);
-					System.out.println(); // 빈 줄 추가
-				} else if (menu == 2) { // 게시글 삭제
-					System.out.println("삭제할 게시글 ID를 입력하세요.");
+					System.out.println("寃뚯떆湲��씠 �벑濡앸릺�뿀�뒿�땲�떎. ID: " + id);
+					System.out.println(); // 鍮� 以� 異붽�
+				} else if (menu == 2) { // 寃뚯떆湲� �궘�젣
+					System.out.println("�궘�젣�븷 寃뚯떆湲� ID瑜� �엯�젰�븯�꽭�슂.");
 					int id = sc.nextInt();
 					board.deletePost(id);
-					System.out.println("게시글이 삭제되었습니다.");
-					System.out.println(); // 빈 줄 추가
-				} else if (menu == 3) { // 게시글 목록 보기
+					System.out.println("寃뚯떆湲��씠 �궘�젣�릺�뿀�뒿�땲�떎.");
+					System.out.println(); // 鍮� 以� 異붽�
+				} else if (menu == 3) { // 寃뚯떆湲� 紐⑸줉 蹂닿린
 					board.showPosts();
-					System.out.println(); // 빈 줄 추가
-				} else if (menu == 0) { // 종료
-					System.out.println("프로그램을 종료합니다.");
+					System.out.println(); // 鍮� 以� 異붽�
+				} else if (menu == 0) { // 醫낅즺
+					System.out.println("�봽濡쒓렇�옩�쓣 醫낅즺�빀�땲�떎.");
 					break;
-					// break 로 해당 프로그램을 종료한다.
+					// break 濡� �빐�떦 �봽濡쒓렇�옩�쓣 醫낅즺�븳�떎.
 				} else {
-					System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
-					System.out.println(); // 빈 줄 추가
-					continue; // 처음으로 돌아간다.
+					System.out.println("�옒紐삳맂 �엯�젰�엯�땲�떎. �떎�떆 �엯�젰�빐二쇱꽭�슂.");
+					System.out.println(); // 鍮� 以� 異붽�
+					continue; // 泥섏쓬�쑝濡� �룎�븘媛꾨떎.
 				}
-			} catch (InputMismatchException e) { // 오류시 처음으로 돌아간다.
-				System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+			} catch (InputMismatchException e) { // �삤瑜섏떆 泥섏쓬�쑝濡� �룎�븘媛꾨떎.
+				System.out.println("�옒紐삳맂 �엯�젰�엯�땲�떎. �떎�떆 �엯�젰�빐二쇱꽭�슂.");
 				System.out.println();
-				sc.nextLine(); // 입력 버퍼를 비워줌
-				continue; // while문의 처음으로 돌아감}
+				sc.nextLine(); // �엯�젰 踰꾪띁瑜� 鍮꾩썙以�
+				continue; // while臾몄쓽 泥섏쓬�쑝濡� �룎�븘媛�}
 			}
 		}
 	}
